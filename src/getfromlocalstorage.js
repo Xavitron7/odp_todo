3be75731ba8141681f8e916ca6dea6ad891c7e8f
Sean Brown <seanbrown82@hotmail.com>
2021-02-28 13:00:02 +0000

    empty LS handling

--- src/getfromlocalstorage.js
+++ src/getfromlocalstorage.js
@@ -2,8 +2,11 @@ import renderProjects from "./renderprojects.js";
 
 //Retrieves list from local storage, turns it into an array, and passes it to the render function.
 const getFromLocalStorage = () => {
-    let projects = JSON.parse(localStorage.getItem("project-list"));
-    renderProjects(projects);
+    if (window.localStorage.length > 0) {
+      let projects = JSON.parse(localStorage.getItem("project-list"));
+    renderProjects(projects);  
+    }
+    
 }
 
 export default getFromLocalStorage;
